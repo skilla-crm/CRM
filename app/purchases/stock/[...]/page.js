f022eb238a129012f4b63165e63ae06082f5a7de
Aleksey <alexholm222@yandex.ru>
2025-05-15 22:49:11 +0300

    fix/склад

---
+++ app/purchases/stock/[...]/page.js
@@ -0,0 +1,28 @@
+
+import { cookies } from 'next/headers'
+import dynamic from 'next/dynamic'
+
+export const metadata = {
+  title: "Закупки"
+};
+
+const DynamicModuleContainer = dynamic(() =>
+  import('@/components/ModuleContainer/ModuleContainer')
+    .then((module) => module.ModuleContainer)
+)
+
+export default async function Stock() {
+  const id = "root_stock"
+  const cookieStore = await cookies()
+  const token = cookieStore.get('token')
+  const role = cookieStore.get('role')
+  const ispro = cookieStore.get('is_pro')
+
+  return (
+    <div id={id} ispro={ispro?.value} role={role?.value} token={`Bearer ${token?.value}`} isskilla="0">
+      <DynamicModuleContainer src={process.env.REACT_APP_URL_STOCK} srcCss={process.env.REACT_APP_URL_STOCK_CSS} id={id} />
+    </div>
+
+
+  );
+}
