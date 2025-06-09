1fda1eafc64c6126669aa9aea0d133e22328d682
alexholm222 <alexholm200@yandex.ru>
2025-06-09 18:31:00 +0300

    fix/

--- middleware.js
+++ middleware.js
@@ -19,18 +19,19 @@ export default function middleware(req) {
     const isBlocked = req.cookies.get('is_blocked')
     const token = req.cookies.get('token')
     const role = req.cookies.get('role')
+    console.log(req.nextUrl.pathname)
 
     if (!token) {
         return NextResponse.redirect("https://lk.skilla.ru/login");
     }
 
-    if (token && role.value === 'accountant') {
-        return NextResponse.redirect("https://lk.skilla.ru/new/orders");
-    }
-
-    if (token && role.value === 'director') {
-        return NextResponse.redirect("https://lk.skilla.ru/new/dashboard");
-    }
+    /*  if (token && role.value === 'accountant') {
+         return NextResponse.redirect("https://lk.skilla.ru/new/orders");
+     }
+ 
+     if (token && role.value === 'director') {
+         return NextResponse.redirect("https://lk.skilla.ru/new/dashboard");
+     } */
 
     if (role.value !== 'director' && role.value !== 'accountant') {
         return NextResponse.redirect("https://lk.skilla.ru")
