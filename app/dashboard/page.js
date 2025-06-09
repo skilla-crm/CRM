15df27a92f4826be93932fc1b03ce9f9b4ad7911
alexholm222 <alexholm200@yandex.ru>
2025-06-09 18:56:11 +0300

    fix/редиректы

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,4 +1,7 @@
-import Iframe from '@/components/Iframe/Iframe'
+import Iframe from '@/components/Iframe/Iframe';
+import { redirect } from 'next/navigation'
+import { cookies } from 'next/headers'
+const role = cookieStore.get('role')
 
 export const metadata = {
   title: "Дашборд"
@@ -7,6 +10,11 @@ export const metadata = {
 export default async function Dashboar() {
   const src = process.env.REACT_APP_URL_DASHBOARD;
   const id = 'root_dashboard'
+
+  if (role?.value === 'accountant') {
+    redirect('/orders')
+  }
+  
   return (
     <Iframe src={src} id={id} />
   );
--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,7 @@ const nextConfig = {
         return [
             {
                 source: '/',
-                destination: '/',
+                destination: '/orders',
                 permanent: true,
             },
 
