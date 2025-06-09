ab3603e34037fd95a2986279fa610403dad42d4e
alexholm222 <alexholm200@yandex.ru>
2025-06-09 19:06:51 +0300

    fix/дашборд

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,20 +1,22 @@
 import Iframe from '@/components/Iframe/Iframe';
 import { redirect } from 'next/navigation'
 import { cookies } from 'next/headers'
-const role = cookieStore.get('role')
+
 
 export const metadata = {
   title: "Дашборд"
 };
 
 export default async function Dashboar() {
+  const cookieStore = await cookies()
+  const role = cookieStore.get('role')
   const src = process.env.REACT_APP_URL_DASHBOARD;
   const id = 'root_dashboard'
 
   if (role?.value === 'accountant') {
     redirect('/orders')
   }
-  
+
   return (
     <Iframe src={src} id={id} />
   );
