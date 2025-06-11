58c01d59ebdeec44861048cd53dbf50ae53eeb64
alexholm222 <alexholm200@yandex.ru>
2025-06-11 10:13:49 +0300

    fix/

--- middleware.js
+++ middleware.js
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-export const testTokens = ['2109|7d9OHVhjO02gY9rrbjV5rTfCpFs4iVShk6TtSrCg', '17|ZLcO2bSQBbExVhlHVsPq6onXF441I4lU2WpHZTGo']
+export const testTokens = ['2109|7d9OHVhjO02gY9rrbjV5rTfCpFs4iVShk6TtSrCg', '17|ZLcO2bSQBbExVhlHVsPq6onXF441I4lU2WpHZTGo', '285|uAvDwNOyod4MzdFBTr0sEEkAQLQsuV9gdCa3sP4o']
 
 
 const protectedRoutes = [
@@ -26,17 +26,17 @@ export default function middleware(req) {
     const test = testTokens.includes(token.value.replace('%7C', '|'))
     console.log(test)
 
-     if (!token) {
-         return NextResponse.redirect("https://lk.skilla.ru/login");
-     }
- 
-     if (role.value !== 'director' && role.value !== 'accountant') {
-         return NextResponse.redirect("https://lk.skilla.ru")
-     }
+    if (!token) {
+        return NextResponse.redirect("https://lk.skilla.ru/login");
+    }
 
-     if (role.value === 'accountant' && !test) {
-         return NextResponse.redirect("https://lk.skilla.ru/accountant")
-     }
+    if (role.value !== 'director' && role.value !== 'accountant') {
+        return NextResponse.redirect("https://lk.skilla.ru")
+    }
+
+    if (role.value === 'accountant' && !test) {
+        return NextResponse.redirect("https://lk.skilla.ru/accountant")
+    }
 
     if (isBlocked?.value === '1' && protectedRoutes.some(el => req.nextUrl.pathname.includes(el))) {
         return NextResponse.redirect(new URL("/new/pay", req.url));
