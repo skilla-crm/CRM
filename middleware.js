9045e0c1c829a9c656880ebc688e8a66e8ef06e1
alexholm222 <alexholm200@yandex.ru>
2025-06-11 09:36:32 +0300

    fix/редирект

--- middleware.js
+++ middleware.js
@@ -1,4 +1,7 @@
 import { NextResponse } from "next/server";
+import { testTokens } from "./constants/exceptions";
+
+
 const protectedRoutes = [
     "/orders",
     "/workers",
@@ -19,14 +22,19 @@ export default function middleware(req) {
     const isBlocked = req.cookies.get('is_blocked')
     const token = req.cookies.get('token')
     const role = req.cookies.get('role')
-    console.log(req.nextUrl.pathname)
+    console.log(req.nextUrl.pathname, token)
+    const test = testTokens.includes(token.value)
 
-    if (!token) {
-        return NextResponse.redirect("https://lk.skilla.ru/login");
-    }
+      if (!token) {
+          return NextResponse.redirect("https://lk.skilla.ru/login");
+      }
+  
+      if (role.value !== 'director' && role.value !== 'accountant') {
+          return NextResponse.redirect("https://lk.skilla.ru")
+      }
 
-    if (role.value !== 'director' && role.value !== 'accountant') {
-        return NextResponse.redirect("https://lk.skilla.ru")
+    if (role.value === 'accountant' && !test) {
+        return NextResponse.redirect("https://lk.skilla.ru/accountant")
     }
 
     if (isBlocked?.value === '1' && protectedRoutes.some(el => req.nextUrl.pathname.includes(el))) {
