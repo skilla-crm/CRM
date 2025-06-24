23b88a42ce30f7c6fae11dd3b035688fbbbea7f1
alexholm222 <alexholm200@yandex.ru>
2025-06-24 13:38:23 +0300

    feature/v26

--- components/ModuleContainer/ModuleContainer.jsx
+++ components/ModuleContainer/ModuleContainer.jsx
@@ -8,7 +8,7 @@ export const ModuleContainer = ({ src, srcCss, id }) => {
   const path = usePathname();
   const searchParamsType = useSearchParams().get('type');
   const searchParamsDate = useSearchParams().get('date');
-  const version = 25;
+  const version = 26;
 
   useEffect(() => {
     if (path === '/orders/create') {
