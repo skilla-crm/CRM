ce8cb4c656f7c8d1f900df214b86395ec627446e
alexholm222 <alexholm222@yandex.ru>
2025-06-30 16:54:35 +0300

    feature/исполнители тестовой группе

--- components/ModuleContainer/ModuleContainer.jsx
+++ components/ModuleContainer/ModuleContainer.jsx
@@ -8,7 +8,7 @@ export const ModuleContainer = ({ src, srcCss, id }) => {
   const path = usePathname();
   const searchParamsType = useSearchParams().get('type');
   const searchParamsDate = useSearchParams().get('date');
-  const version = 31;
+  const version = 33;
 
   useEffect(() => {
     if (path === '/orders/create') {
--- constants/exceptions.js
+++ constants/exceptions.js
@@ -1,2 +1,12 @@
 export const oneCityTokens = ['616|25By3OpcgVqtNzKsuERgLAsxfLNzhE2wjljjRIUk', '435|I8njfkTAlssfn818ukCNyMqEjjFESRpvbz549Oxr']
-export const testTokens = ['2109|7d9OHVhjO02gY9rrbjV5rTfCpFs4iVShk6TtSrCg', '17|ZLcO2bSQBbExVhlHVsPq6onXF441I4lU2WpHZTGo']
+export const testTokens = [
+    '2109|7d9OHVhjO02gY9rrbjV5rTfCpFs4iVShk6TtSrCg',
+    '17|ZLcO2bSQBbExVhlHVsPq6onXF441I4lU2WpHZTGo',
+    '439|Q1JiQ0EUKBcuLo6HgYI48ZTNAnU4muAgyE0Ufn9G',
+    '54|mk7iYZbPBHCX8tWjarAuggBaWVNSHpqDSVTMxs6G',
+    '86|ChX7evHyxw11ATIwr2UT75WZqxfFmkLTz6gRaJYf',
+    '59|RSpZcIkgb9bZ2m4FzrBtfalbU6a3mfBYWB0nMpcN',
+    '2695|bPfpYkzu7mTIBNRAwCf3T0o2rx8dAcobejtzY5RX',
+    '2738|2zUKquaqQj9b5XrLIxXGx4JwnPNRMUOG2jWjmZsz',
+    '721|l2z1hQZTofgoE3Cigge8l1cfaflYmrvMm1GW3VLk'
+]
