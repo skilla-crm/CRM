4b768efe6ea6764e6ef421f3824b9bcb7970c797
alexholm222 <alexholm222@yandex.ru>
2025-08-01 11:28:43 +0300

    v59

---
+++ app/accountan-docs/[...id]/page.js
@@ -0,0 +1,30 @@
+
+import { cookies } from 'next/headers'
+import dynamic from 'next/dynamic'
+
+export const metadata = {
+  title: "Банк"
+};
+
+
+const DynamicModuleContainer = dynamic(() =>
+  import('@/components/ModuleContainer/ModuleContainer')
+    .then((module) => module.ModuleContainer)
+)
+
+export default async function AccountanDocs() {
+  const id = "root_accountant_docs"
+  const cookieStore = await cookies()
+  const token = cookieStore.get('token')
+  const role = cookieStore.get('role')
+  const ispro = cookieStore.get('is_pro')
+
+  return (
+    <>
+      <div id={id} ispro={ispro?.value} role={role?.value} token={`Bearer ${token?.value}`}>
+        <DynamicModuleContainer src={process.env.REACT_APP_URL_DOCS} srcCss={process.env.REACT_APP_URL_DOCS_CSS} id={id} />
+      </div>
+    </>
+
+  );
+}
