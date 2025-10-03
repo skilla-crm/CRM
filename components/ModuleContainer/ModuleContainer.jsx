63db8075cf7a18b6a9afaa82eabc8dd0ccbdc2e0
alexholm222 <alexholm222@yandex.ru>
2025-10-03 16:51:12 +0300

    v118

--- components/ModuleContainer/ModuleContainer.jsx
+++ components/ModuleContainer/ModuleContainer.jsx
@@ -8,7 +8,7 @@ export const ModuleContainer = ({ src, srcCss, id }) => {
   const path = usePathname();
   const searchParamsType = useSearchParams().get('type');
   const searchParamsDate = useSearchParams().get('date');
-  const version = 117;
+  const version = 118;
 
 
   useEffect(() => {
