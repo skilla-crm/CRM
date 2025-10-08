d0993185179d83e16bc8bea16f6e29507fd00e0d
Aleksey <alexholm222@yandex.ru>
2025-10-08 14:25:58 +0300

    v127

--- middleware.js
+++ middleware.js
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-export const testTokens = ['17', '200', '1001', '443', '1076', '1036', '1336', '1322', '1327', '127', '1083', '1122', '1335']
+export const testTokens = ['17', '200', '1001', '443', '1076', '1036', '1336', '1322', '1327', '127', '1083', '1122', '1335', '8']
 
 
 const protectedRoutes = [
