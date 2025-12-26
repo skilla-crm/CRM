eef068dd472fdeee7946ada9ed2d5a028377a56f
alexholm222 <alexholm222@yandex.ru>
2025-12-26 10:59:27 +0300

    fix

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -3,7 +3,7 @@ import { cookies } from 'next/headers'
 import dynamic from 'next/dynamic'
 
 export const metadata = {
-  title: "События"
+  title: "Дашборд"
 };
 
 const DynamicModuleContainer = dynamic(() =>
