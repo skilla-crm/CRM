fd7fb8231973e14adfaa9c805bffbd1946f5f4b2
alexholm222 <alexholm222@yandex.ru>
2026-02-20 11:52:26 +0300

    add project page

---
+++ app/(with-menu)/project/page.js
@@ -0,0 +1,10 @@
+import ModuleConnection from '@/components/ModuleConnection/ModuleConnection';
+
+export const metadata = {
+  title: "Проекты"
+};
+
+const Project = () => <ModuleConnection id={'project'} />
+
+export default Project
+
